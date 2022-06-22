ede679f76f6aed57a347b60b27bd81b999805929
Diego Urrego <diegourrg@gmail.com>
2022-06-22 17:36:07 -0500

    Sección Hook useLayoutEffect terminada.

---
+++ src/05-useLayoutEffect/Layout.jsx
@@ -0,0 +1,39 @@
+import { useCounter, useFetch } from '../hooks';
+import { LoadingQuote, Quote } from '../03-examples';
+
+export const Layout = () => {
+  const { counter, increment, decrement } = useCounter(1);
+
+  const { data, isLoading, hasError } = useFetch(
+    `https://www.breakingbadapi.com/api/quotes/${counter}`
+  );
+
+  const { author, quote } = !!data && data[0];
+
+  return (
+    <>
+      <h1>Breaking Bad Quotes</h1>
+      <hr />
+
+      {isLoading ? <LoadingQuote /> : <Quote quote={quote} author={author} />}
+
+      {counter > 1 && (
+        <button
+          className='btn btn-primary'
+          disabled={isLoading}
+          onClick={() => decrement()}
+        >
+          Previous Quote
+        </button>
+      )}
+
+      <button
+        className='btn btn-primary'
+        disabled={isLoading}
+        onClick={() => increment()}
+      >
+        Next Quote
+      </button>
+    </>
+  );
+};
