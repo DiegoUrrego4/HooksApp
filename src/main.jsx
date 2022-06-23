7fff87713b21206acf320cfdcbac5c5080f2001e
Diego Urrego <diegourrg@gmail.com>
2022-06-23 17:43:04 -0500

    Memorize entendido.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Layout } from './05-useLayoutEffect/Layout';
+import { Memorize } from './06-memos/Memorize';
+// import { Layout } from './05-useLayoutEffect/Layout';
 // import { FocusScreen } from './04-useRef/FocusScreen';
 // import { MultipleCustomHook } from './03-examples/MultipleCustomHook';
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
@@ -13,6 +14,6 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <Layout />
+    <Memorize />
   // </React.StrictMode>
 );
