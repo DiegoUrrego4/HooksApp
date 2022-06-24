ea393c0ab0d4cbb91ce0c7a9c01dfca31a8a8d71
Diego Urrego <diegourrg@gmail.com>
2022-06-24 16:35:32 -0500

    Refactorización componente <TodoApp /> completa.

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -1,46 +1,14 @@
-import { useEffect, useReducer } from 'react';
-import { TodoAdd, TodoList, todoReducer } from './';
-
-const initialState = [];
-
-const init = () => {
-  return JSON.parse(localStorage.getItem('todos')) || [];
-};
+import { useTodos } from '../hooks/useTodos';
+import { TodoAdd, TodoList } from './';
 
 export const TodoApp = () => {
-  const [todos, dispatch] = useReducer(todoReducer, initialState, init);
-
-  useEffect(() => {
-    localStorage.setItem('todos', JSON.stringify(todos));
-  }, [todos]);
-
-  const handleNewTodo = (todo) => {
-    const action = {
-      type: '[TODO] Add Todo',
-      payload: todo,
-    };
-    // Para enviar la acción al reducer debemos usar el dispatch
-    dispatch(action);
-  };
-
-  const handleDeleteTodo = (todoId) => {
-    dispatch({
-      type: '[TODO] Remove Todo',
-      payload: todoId,
-    });
-  };
-
-  const handleToggleTodo = (todoId) => {
-    dispatch({
-      type: '[TODO] Toggle Todo',
-      payload: todoId,
-    });
-  };
+  const { todos, handleNewTodo, handleDeleteTodo, handleToggleTodo } =
+    useTodos();
 
   return (
     <>
       <h1>
-        Todo App 10, <small>pendientes: 2</small>{' '}
+        Todo App 10, <small>pendientes: 2</small>
       </h1>
       <hr />
 
--- src/hooks/useTodos.js
+++ src/hooks/useTodos.js
@@ -1,3 +1,44 @@
-export const useTodos = () => {
-  return {}
-}
+import { useEffect, useReducer } from 'react';
+import { todoReducer } from '../08-useReducer';
+
+export const useTodos = (initialState = []) => {
+  const init = () => {
+    return JSON.parse(localStorage.getItem('todos')) || [];
+  };
+
+  const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  const handleNewTodo = (todo) => {
+    const action = {
+      type: '[TODO] Add Todo',
+      payload: todo,
+    };
+    // Para enviar la acción al reducer debemos usar el dispatch
+    dispatch(action);
+  };
+
+  const handleDeleteTodo = (todoId) => {
+    dispatch({
+      type: '[TODO] Remove Todo',
+      payload: todoId,
+    });
+  };
+
+  const handleToggleTodo = (todoId) => {
+    dispatch({
+      type: '[TODO] Toggle Todo',
+      payload: todoId,
+    });
+  };
+
+  return {
+    todos,
+    handleNewTodo,
+    handleDeleteTodo,
+    handleToggleTodo,
+  };
+};
